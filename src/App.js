0b8314748a44410302cbd39b3b5e42cf620f7ff4
memizzos <4321nitro1234@gmail.com>
2019-04-02 19:39:27 +0200

    cokolwiek

--- src/App.js
+++ src/App.js
@@ -1,28 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react'
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(){
+    super()
+    this.state = {
+    loading: false,
+    postac: {},
+    }
   }
+
+  componentDidMount(){
+    this.setState({ loading: true})
+    fetch("https://swapi.co/api/people/10/")
+    .then( response => response.json() )
+    .then (data => this.setState({ postac:data, loading:false}))
+    .catch(error => console.log(error))
+    }
+  
+
+
+  render(){
+        const text = this.state.loading ? "Loading..." : this.state.postac.name
+    
+    return(
+      <div>
+        <h1>{text}</h1>
+      </div>
+    )
 }
 
-export default App;
+}
+
+
+export default App
